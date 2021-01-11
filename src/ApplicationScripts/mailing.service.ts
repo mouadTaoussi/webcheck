7239437cc4fcd52e6fb6cc83caef140ab947a194
mouadtaoussi <mouadtaoussi0@gmail.com>
2021-01-11 23:03:51 +0100

    Finishing the feature (Display the website performance in graph)

--- src/ApplicationScripts/mailing.service.ts
+++ src/ApplicationScripts/mailing.service.ts
@@ -40,7 +40,109 @@ class EmailService implements EmailService {
 				auth: { user: application_config.email, pass: application_config.password }
 			});
 			// Check the language the user set in the app to send the email appropriated to his language
-			let mailTemplate;
+			let mailTemplate = `
+				<!DOCTYPE html>
+					<html>
+					<head>
+						<title>Features</title>
+						<style>
+							@font-face {
+							    font-family: poppins;
+							    src: url("https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&family=Ubuntu:wght@400;500&display=swap");
+							}
+						</style>
+					</head>
+					<body>
+						<div class="container">
+							<header class="header">
+								<img class="image" src="https://webcheck1.herokuapp.com/img/logo_light.9d3a344c.svg">
+							</header>
+							<div>
+								<p class="greetings" >Hello ${users[i].name}</p>
+								<h1 class="title">New Features and fixes are released!</h1>
+								<div class="doted-list">
+									<ul>
+										<li>You can get analyses about your websits</li>
+										<li>You can get a lot of things</li>
+										<li>You can get a lot of things</li>
+										<li>You can get a lot of things</li>
+									</ul>
+								</div>
+								<div class="footer">
+									<ul>
+										<li>
+											<a class="link" href="https://webcheck1.herokuapp.com/#/privacypolicy">
+												Privacy and policy
+											</a>
+										</li>
+										<li>
+											<a class="link" href="https://webcheck1.herokuapp.com/#/dashboard">
+												Open app
+											</a>
+										</li>
+										<li>
+											<a class="link" href="https://webcheck1.herokuapp.com/#/about">
+												About
+											</a>
+										</li>
+									</ul>
+								</div>
+							</div>
+							<style>
+								body {
+									background-color: rgba(0,0,0,.1);
+									font-family: tahoma,verdana,Courier,helvetica;
+								}
+								.image {
+									width: 200px;
+									height: 100px;
+								}
+								.container {
+									width: 80%;
+									/*height: 500p*/
+									padding: 20px;
+									margin: 0 auto;
+									background-color: white;
+
+								}
+								.doted-list ul {
+									padding-left: 20px;
+								}
+								.doted-list ul li {
+									margin-top: 10px;
+								}
+								.greetings {
+									font-family: Courier;
+									font-weight: bolder;
+									font-size: 20px;
+									margin: 0;
+									color: rgba(0,0,0,.7);
+
+								}
+								.title {
+									margin: 0;
+								}
+								.header {
+
+								}
+								.footer ul {
+									margin-top: 10px;
+									padding-left: 0;
+									list-style: none;
+								}
+								.footer ul li {
+									display: inline;
+									margin: 10px;
+								}
+								.link {
+									color: rgba(0,0,0,.5);
+									text-decoration: none;
+								}
+							</style>
+						</div>
+					</body>
+					</html>
+			`;
 
 			// send it!
 			transporter.sendMail({
