e59be2c8f24fcf2e2127b61e7ebbd942137b2762
mouadtaoussi <mouadtaoussi0@gmail.com>
2021-01-12 11:21:05 +0100

    Finishing the feature (Display the website performance in graph)

--- wc-front-end/api.config.js
+++ wc-front-end/api.config.js
@@ -1,6 +1,6 @@
 const api_config = {
-	// apiPath   : "/"  // API Origin <SERVER SIDE ENDPOINT> 
-	apiPath   : "http://localhost:8000/"
+	apiPath   : "/"  // API Origin <SERVER SIDE ENDPOINT> 
+	// apiPath   : "http://localhost:8000/"
 }
 // http://localhost:8000
 export default api_config;
