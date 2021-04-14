f5a1cb13d7f720681b21623ca23647471365175b
mouadtaoussi <mouadtaoussi0@gmail.com>
2021-04-14 20:06:58 +0100

    changing the logo

--- src/Authentication/Authentication.corsPolicy.ts
+++ src/Authentication/Authentication.corsPolicy.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 
 export default function cors(request: Request, response:any,next:NextFunction){
 
-	response.header("Access-Control-Allow-Origin", '*'); /* https://projectgigs.now.sh*/
+	response.header("Access-Control-Allow-Origin", 'https://webcheck.vercel.app'); /* https://projectgigs.now.sh*/
 	response.header("Access-Control-Allow-Credentials", true);
 	response.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
 	response.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,Content-Type,Authorization');
