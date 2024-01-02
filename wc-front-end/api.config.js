d8104664e9f30faf317b138529a368916a2ebfa6
mouadTaoussi <mouadtaoussi0@gmail.com>
2024-01-02 21:41:58 +0100

    Migrate backend host from heroku

--- wc-front-end/api.config.js
+++ wc-front-end/api.config.js
@@ -1,7 +1,7 @@
 const api_config = {
 	// apiPath   : "https://webcheck1.herokuapp.com/",  // API Origin <SERVER SIDE ENDPOINT> 
 	apiPath   : "https://dark-gray-earthworm-hem.cyclic.app/",
-	// apiPath   : "http://localhost:8000/"
+	// apiPath   : "http://localhost:8000/" 
 }
 // http://localhost:8000
 export default api_config;
